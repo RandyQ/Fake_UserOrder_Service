359afe27e9d778c632596c27604203a4e97f7198
Randy <rcq1@students.uwf.edu>
2019-11-22 13:52:21 -0600

    completed fake ordering service

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Fake_UserOrder_Service
+
+To start the Fake Ordering Service, call the route 'localhost:3002/start' (or 18.223.169.23/start).
+The start route '/start' begins the test harness.
--- app.js
+++ app.js
@@ -10,41 +10,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Handle JSON in the body
 app.use(require('body-parser').json());
 
-app.get('/', function(req, res) {
-    axios.post('http://localhost:3000/purchase', {
-    item: 'Hotdog',
-    quantity: '3'
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-  res.status(200).send("post successful");
+app.get('/start', function (req, res) {
+    setInterval(() => axios.post('http://localhost:3000/purchase', {
+        item: 'Hotdog',
+        quantity: '10'
+    })
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        }), 2000);
+    res.status(200).send("Fake Ordering Service has been initiated...");
 });
 
+function test() {
+
+    return 0;
+}
+
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.status(404);
     res.json({
-        "status" : "error",
-        "message" : "This page can not be found."
+        "status": "error",
+        "message": "This page can not be found."
     });
-  });
+});
 
-  // 500 error handler (middleware)
-app.use(function(error, req, res, next){
+// 500 error handler (middleware)
+app.use(function (error, req, res, next) {
     console.error(error.stack);
     res.status(500);
     res.json({
-        "status" : "error",
-        "message" : "An internal error has occurred."
+        "status": "error",
+        "message": "An internal error has occurred."
     });
-  });
-  
-  app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-      app.get('port') + '; press Ctrl-C to terminate.' );
-  });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.');
+});
